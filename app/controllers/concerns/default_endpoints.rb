b8dcaabe53815301f6e38d69299327e121f4afb8
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-07 17:09:40 +0200

    Implement default endpoints handler

---
+++ app/controllers/concerns/default_endpoints.rb
@@ -0,0 +1,21 @@
+module DefaultEndpoints
+  def default_handler
+    lambda do |model|
+      model.created do |result|
+        render jsonapi: result[:model],
+        **result[:renderer_options],
+        status: :created
+      end
+
+      model.invalid do |result|
+        render jsonapi_errors: result['contract.default'].errors,
+        class: { 'Reform::Form::ActiveModel::Errors': JSONAPI::Rails::SerializableActiveModelErrors },
+        status: :unprocessable_entity
+      end
+    end
+  end
+
+  def endpoint(operation_class, _options = {}, &block)
+    Endpoint.call(operation_class, default_handler, { params: params.to_unsafe_h }, &block)
+  end
+end
