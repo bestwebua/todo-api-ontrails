640859698264aa557ccbcf7ef582bd8237abcc74
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-07 17:54:51 +0200

    Refactor routes namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  scope :auth do
-    post '/', to: 'users#create'
-    post '/remove_me', to: 'users#destroy'
+  scope :api do
+    scope :auth do
+      post '/', to: 'users#create'
+      post '/remove_me', to: 'users#destroy'
 
-    # post '/sign_in', to: 'authentication#create'
-    # get '/sign_out', to: 'authentication#destroy'
+      # post '/sign_in', to: 'authentication#create'
+      # get '/sign_out', to: 'authentication#destroy'
+    end
   end
 end
