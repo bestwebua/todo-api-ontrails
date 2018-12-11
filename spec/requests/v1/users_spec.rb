90a0c1c1dd9f9310b977dbc10ff9a46533b730b4
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-11 16:59:27 +0200

    Fix ActionDispatch::Http::Parameters::ParseError: 765

--- spec/requests/v1/users_spec.rb
+++ spec/requests/v1/users_spec.rb
@@ -2,13 +2,12 @@
 require 'rails_helper'
 
 RSpec.describe 'V1::User API', type: :request do
-
   let(:user) { build(:user) }
   let!(:valid_attributes) { attributes_for(:user, password_confirmation: user.password) }
 
   describe 'POST /api/auth' do
     context 'valid request' do
-      before { post '/api/auth', params: valid_attributes, headers: valid_headers }
+      before { post '/api/auth', params: valid_attributes.to_json, headers: valid_headers }
 
       it 'creates a new user' do
         expect(response).to have_http_status(201)
--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -1,7 +1,7 @@
 module ControllerSpecHelper
   def valid_headers(version = 'v1')
     {
-      'Content-Type': 'application/vnd.api+json'
+      'Content-Type': 'application/json'
     }.merge('Accept': "application/vnd.api.#{version}")
   end
 end
