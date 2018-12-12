afe8eb32578001b60721661ce8c9f2657a6ff5d1
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-12 17:22:45 +0200

    Implement documentation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get '/', to: redirect('api/documentation')
+
   scope :api, module: :v1, constraints: Api::VersionService.new(:v1, true) do
     namespace :users do
       resource :registration, only: :create
     end
-    # post '/sign_in', to: 'authentication#create'
-    # get '/sign_out', to: 'authentication#destroy'
+
+    resources :documentation, only: :index
   end
 end
