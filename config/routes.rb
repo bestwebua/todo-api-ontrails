2a44141c37fce719b524345e38e560971d5b8cd7
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-14 15:40:26 +0200

    Refactor documentation controller actions, use endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/', to: redirect('api/documentation/v1')
 
   scope :api do
-    get '/documentation/:version', to: 'documentation#index'
+    get '/documentation/:version', to: 'documentation#show'
   end
 
   scope :api, module: :v1, constraints: Api::VersionService.new(:v1, true) do
