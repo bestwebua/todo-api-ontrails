ff92c53497dde8d17875f1961c4e30b487cd2da3
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-16 13:00:42 +0200

    Implement EmailToken service

---
+++ app/concepts/v1/lib/service/email_token.rb
@@ -0,0 +1,17 @@
+module V1::Lib::Service
+  class EmailToken
+    class << self
+      DEFAULT_TOKEN_EXPIRATION = 24.hours.from_now
+      HMAC_SECRET = Rails.application.credentials.secret_jwt_encryption_key
+
+      def create(payload, exp = DEFAULT_TOKEN_EXPIRATION)
+        payload[:exp] = exp.to_i
+        JWT.encode(payload, HMAC_SECRET)
+      end
+
+      def read(token)
+        JWT.decode(token, HMAC_SECRET).first
+      end
+    end
+  end
+end
