a8141b83557af09414db392e6a0de3b42daf8ba7
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-16 13:02:13 +0200

    Add CreateConfirmation step

--- app/concepts/v1/users/registrations/operation/create.rb
+++ app/concepts/v1/users/registrations/operation/create.rb
@@ -4,6 +4,7 @@ module V1::Users::Registrations::Operation
     step Contract::Build(constant: V1::Users::Registrations::Contract::Create)
     step Contract::Validate()
     step Contract::Persist()
+    step V1::Lib::Step::Mailers::Users::CreateConfirmation
     step :renderer_options
 
     def renderer_options(ctx, **)
