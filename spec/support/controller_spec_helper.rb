32df786835c1f276d5a820dc8fd0127e888a25fe
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-20 15:05:01 +0200

    Add user_id default arg

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -5,13 +5,13 @@ module ControllerSpecHelper
     }.merge('Accept': "application/vnd.api.#{version}")
   end
 
-  def valid_token_generator(token_type, user_id)
+  def valid_token_generator(token_type, user_id = rand(1..10))
     case token_type
     when :email_token then V1::Lib::Service::EmailToken.create(user_id: user_id)
     end
   end
 
-  def invalid_token_generator(token_type, user_id)
+  def invalid_token_generator(token_type, user_id = rand(1..10))
     case token_type
     when :email_token then V1::Lib::Service::EmailToken.create({ user_id: user_id }, Time.now.to_i - 10)
     end
