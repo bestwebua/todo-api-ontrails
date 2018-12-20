5ea30e01a8d322f9425f670a885b43ae89680db4
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-20 15:05:30 +0200

    Refactor EmailConfirmationWorker tests

--- spec/workers/v1/users/email_confirmation_worker_spec.rb
+++ spec/workers/v1/users/email_confirmation_worker_spec.rb
@@ -1,10 +1,10 @@
 describe V1::Users::EmailConfirmationWorker, type: :worker do
-  let(:user) { create(:user) }
-  let(:token) { valid_token_generator(:email_token, user.id) }
+  let(:email) { Faker::Internet.email }
+  let(:token) { valid_token_generator(:email_token) }
   let(:user_verification_path) { Faker::Internet.slug }
 
   it 'should call UserMailer' do
     expect(UserMailer).to receive_message_chain(:confirmation, :deliver_now)
-    subject.perform(user, token, user_verification_path)
+    subject.perform(email, token, user_verification_path)
   end
 end
