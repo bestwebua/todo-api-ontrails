db010b2308bbaa49d0e3da5b87c305ee331cd0e5
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-21 15:58:07 +0200

    Add Users::CreateConfirmation step tests

---
+++ spec/concepts/v1/lib/step/mailers/users/create_confirmation_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe V1::Lib::Step::Mailers::Users::CreateConfirmation do
+  let(:params) { {} }
+  let(:user) { create(:user) }
+
+  describe '.call' do
+    after { described_class.call(params, model: user) }
+
+    specify { expect(V1::Lib::Service::EmailToken).to receive(:create) }
+    specify { expect(Rails).to receive_message_chain(:application, :config, :user_verification_path) }
+    specify { expect(V1::Users::EmailConfirmationWorker).to receive(:perform_async) }
+  end
+end
