ed212363860738698a289004aefab1dedd366f1b
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-21 15:01:55 +0200

    Add subject localization

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def confirmation(email, token, path)
     @email = email
     @confirmation_url = URI.parse("#{path}?email_token=#{token}").to_s
-    mail(to: @email, subject: 'Email confirmation')
+    mail(to: @email, subject: I18n.t('user_mailer.subject'))
   end
 end
