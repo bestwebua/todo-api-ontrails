357c2160c639b95647fb5a2e55629d7fc52082fb
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-21 14:12:58 +0200

    Refactor EmailToken service

--- app/concepts/v1/lib/service/email_token.rb
+++ app/concepts/v1/lib/service/email_token.rb
@@ -1,17 +1,23 @@
 module V1::Lib::Service
   class EmailToken
-    class << self
-      DEFAULT_TOKEN_EXPIRATION = 24.hours.from_now
-      HMAC_SECRET = Rails.application.credentials.secret_jwt_encryption_key
+    DEFAULT_TOKEN_EXPIRATION = 24.hours.from_now
+    HMAC_SECRET = Rails.application.credentials.secret_jwt_encryption_key
 
+    class << self
       def create(payload, exp = DEFAULT_TOKEN_EXPIRATION)
+        check_secret_key
         payload[:exp] = exp.to_i
         JWT.encode(payload, HMAC_SECRET)
       end
 
       def read(token)
+        check_secret_key
         body = JWT.decode(token, HMAC_SECRET).first
         HashWithIndifferentAccess.new(body)
+      end
+
+      def check_secret_key
+        raise 'Secret key not assigned' unless HMAC_SECRET
       end
     end
   end
