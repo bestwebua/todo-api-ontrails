04618837c7497d9f245b7d0d15c6e7956d20bdce
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-23 13:33:39 +0200

    Complete users verification operation confirm tests

---
+++ spec/concepts/v1/users/verifications/operation/confirm_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe V1::Users::Verifications::Operation::Confirm do
+  subject(:result) { described_class.call(params: params) }
+
+  let(:user) { create(:user) }
+  let(:valid_email_token) { valid_token_generator(:email_token, user.id) }
+  let(:valid_params) { { email_token: valid_email_token } }
+
+  describe 'Success' do
+    let(:params) { valid_params }
+
+    it 'verifies user' do
+      expect(result).to be_success
+      # expect { result }.to change { user.verified }.from(false).to(true)
+    end
+  end
+
+  describe 'Fail' do
+    context 'invalid token' do
+      let(:invalid_email_token) { invalid_token_generator(:email_token, user.id) }
+      let(:params) { valid_params.merge(email_token: invalid_email_token) }
+      let(:errors) { { email_token: I18n.t('errors.invalid_token') } }
+
+      include_examples 'has validation errors'
+    end
+
+    context 'user already verified' do
+      let(:params) { valid_params }
+      before { user.toggle!(:verified) }
+      let(:errors) { { user_account: I18n.t('errors.user_already_verified') } }
+
+      include_examples 'has validation errors'
+    end
+  end
+end
--- spec/support/concept_spec_helper.rb
+++ spec/support/concept_spec_helper.rb
@@ -1,6 +1,9 @@
 module ConceptSpecHelper
   def match_validation_errors(operation_result, error_messages)
     expect(operation_result).to be_failure
-    expect(operation_result['contract.default'].errors.messages).to match(error_messages)
+    errors =
+      operation_result['contract.default'] ?
+        operation_result['contract.default'].errors.messages : operation_result[:errors]
+    expect(errors).to match(error_messages)
   end
 end
