23145bc71fa6adf50d7f52a0861a599b5597e8a2
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-23 11:27:26 +0200

    Complete users registration operation create tests

---
+++ spec/concepts/v1/users/registrations/operation/create_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe V1::Users::Registrations::Operation::Create do
+  subject(:result) { described_class.call(params: params) }
+  let(:user) { build(:user) }
+  let(:valid_params) do
+    {
+      email: user.email,
+      password: user.password,
+      password_confirmation: user.password
+    }
+  end
+
+  describe 'Success' do
+    let(:params) { valid_params }
+
+    before do
+      allow(V1::Lib::Step::Mailers::Users::CreateConfirmation).to receive(:call).and_return(true)
+    end
+
+    it 'creates user' do
+      expect { result }.to change(User, :count).from(0).to(1)
+      expect(result[:model]).to be_persisted
+      expect(result).to be_success
+    end
+
+    it 'calls shared step' do
+      expect(V1::Lib::Step::Mailers::Users::CreateConfirmation).to receive(:call)
+      result
+    end
+  end
+
+  describe 'Fail' do
+    context 'empty keys' do
+      let(:params) { {} }
+      let(:errors) do
+        {
+          email: ["can't be blank"],
+          password: ["can't be blank", 'is too short (minimum is 6 characters)']
+        }
+      end
+
+      include_examples 'has validation errors'
+    end
+
+    context 'different passwords' do
+      let(:params) { valid_params.merge(password_confirmation: '') }
+      let(:errors) { { password_confirmation: [I18n.t('errors.password_mismatch')] } }
+
+      include_examples 'has validation errors'
+    end
+
+    context 'email already exists' do
+      let(:existing_user) { create(:user) }
+      let(:params) { valid_params.merge(email: existing_user.email) }
+      let(:errors) { { email: ['has already been taken'] } }
+
+      include_examples 'has validation errors'
+    end
+  end
+end
