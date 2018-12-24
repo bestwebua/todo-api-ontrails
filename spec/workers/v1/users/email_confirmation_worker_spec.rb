8fd1c182069e7f2c57064ac283f03a5bbfcc09c7
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-24 10:43:09 +0200

    Refactor

--- spec/workers/v1/users/email_confirmation_worker_spec.rb
+++ spec/workers/v1/users/email_confirmation_worker_spec.rb
@@ -1,9 +1,12 @@
+require 'rails_helper'
+
 describe V1::Users::EmailConfirmationWorker, type: :worker do
   let(:email) { Faker::Internet.email }
   let(:token) { valid_token_generator(:email_token) }
   let(:user_verification_path) { Faker::Internet.slug }
 
-  it 'should call UserMailer' do
+  it 'calls UserMailer' do
+    allow(UserMailer).to receive_message_chain(:confirmation, :deliver_now)
     expect(UserMailer).to receive_message_chain(:confirmation, :deliver_now)
     subject.perform(email, token, user_verification_path)
   end
