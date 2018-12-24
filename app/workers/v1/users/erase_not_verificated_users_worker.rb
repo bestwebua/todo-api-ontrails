70340b39c5e8e4d5b2c5b337fdb908bc69d56f74
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-24 09:40:52 +0200

    Move sidekiq cron job to app initializer

--- app/workers/v1/users/erase_not_verificated_users_worker.rb
+++ app/workers/v1/users/erase_not_verificated_users_worker.rb
@@ -9,8 +9,3 @@ module V1
     end
   end
 end
-
-Sidekiq::Cron::Job.create(
-  name: 'Erase not verificated users - every 24h',
-  cron: '* */24 * * *',
-  class: 'V1::Users::EraseNotVerificatedUsersWorker')
