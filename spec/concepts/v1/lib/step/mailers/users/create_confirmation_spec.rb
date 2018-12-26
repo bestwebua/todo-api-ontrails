d7483fb659ad902b8b9db839cbb9601bf810f063
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-26 15:27:22 +0200

    Refactor

--- spec/concepts/v1/lib/step/mailers/users/create_confirmation_spec.rb
+++ spec/concepts/v1/lib/step/mailers/users/create_confirmation_spec.rb
@@ -1,20 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe V1::Lib::Step::Mailers::Users::CreateConfirmation do
-  let(:params) { {} }
-  let(:user) { create(:user) }
-
   describe '.call' do
-    before do
-      allow(V1::Lib::Service::EmailToken).to receive(:create)
-      allow(Rails).to receive_message_chain(:application, :config, :user_verification_path)
-      allow(V1::Users::EmailConfirmationWorker).to receive(:perform_async)
+    let(:params) { {} }
+
+    it '' do
+      expect(V1::Lib::Service::EmailToken).to receive(:create).and_return('token')
+      expect(Rails).to receive_message_chain(:application, :config, :user_verification_path).and_return('verification_path')
+      expect(V1::Users::EmailConfirmationWorker).to receive(:perform_async).and_return(true)
+      described_class.call(params, model: create(:user))
     end
-
-    after { described_class.call(params, model: user) }
-
-    specify { expect(V1::Lib::Service::EmailToken).to receive(:create) }
-    specify { expect(Rails).to receive_message_chain(:application, :config, :user_verification_path) }
-    specify { expect(V1::Users::EmailConfirmationWorker).to receive(:perform_async) }
   end
 end
