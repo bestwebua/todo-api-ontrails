d076ab4819bb8ca30c98dcb500c183891d094603
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-26 15:38:39 +0200

    Typo

--- spec/concepts/v1/lib/step/mailers/users/create_confirmation_spec.rb
+++ spec/concepts/v1/lib/step/mailers/users/create_confirmation_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe V1::Lib::Step::Mailers::Users::CreateConfirmation do
   describe '.call' do
     let(:params) { {} }
 
-    it '' do
+    it 'calls classes chain' do
       expect(V1::Lib::Service::EmailToken).to receive(:create).and_return('token')
       expect(Rails).to receive_message_chain(:application, :config, :user_verification_path).and_return('verification_path')
       expect(V1::Users::EmailConfirmationWorker).to receive(:perform_async).and_return(true)
