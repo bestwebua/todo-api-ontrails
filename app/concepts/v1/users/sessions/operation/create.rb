a32902a48fedbc594c96f264bfd769bfb5698696
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-12-27 17:36:23 +0200

    Add create user session step

--- app/concepts/v1/users/sessions/operation/create.rb
+++ app/concepts/v1/users/sessions/operation/create.rb
@@ -1,11 +1,33 @@
 module V1::Users::Sessions::Operation
   class Create < Trailblazer::Operation
+    step V1::Lib::Step::Error::InitializeCustomErrors
     step V1::Lib::Step::Auth::AuthenticateUser
     fail :invalid_credentials, fail_fast: true
 
+    step :user_verified?
+    fail :user_not_verified, fail_fast: true
+
+    step :create_session
+
+    def create_session(ctx, **)
+      user = ctx[:model]
+      payload = { user_id: user.id }
+      session = JWTSessions::Session.new(payload: payload,
+                                         refresh_by_access_allowed: true,
+                                         namespace: "user_#{user.id}")
+      ctx[:tokens] = session.login
+    end
+
     def invalid_credentials(ctx, **)
-      ctx[:errors] ||= {}
-      ctx[:errors][:credentials] = [I18n.t('errors.invalid_credentials')]
+      V1::Lib::Service::AddCustomError.(ctx, :unauthorized, credentials: I18n.t('errors.invalid_credentials'))
+    end
+
+    def user_verified?(ctx, **)
+      ctx[:model].verified?
+    end
+
+    def user_not_verified(ctx, **)
+      V1::Lib::Service::AddCustomError.(ctx, :unauthorized, user_account: I18n.t('errors.user_not_verified'))
     end
   end
 end
